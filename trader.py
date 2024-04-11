8fbdc02ae244a2021e6185a435088f015d8da808
Woodsyosh <98988677+Woodsyosh@users.noreply.github.com>
2024-04-11 18:23:18 +0200

    Nettoyage

--- trader.py
+++ trader.py
@@ -19,92 +19,75 @@ class Trader:
 
     old_starfruit_asks = []
     old_starfruit_bids = []
-
-    def extract_market_values(self, order_prices, is_buy_order=False):
-        """
-        Extracts total volume and best price from the given orders.
-        """
-        total_volume = 0
-        best_price = -1
-        max_volume = -1
-
-        for price, volume in order_prices.items():
-            if not is_buy_order:
-                volume *= -1
-            total_volume += volume
-            if total_volume > max_volume:
-                max_volume = volume
-                best_price = price
-        
-        return total_volume, best_price
     
     def compute_orders_amethysts(self, state: TradingState):
         order_depth: OrderDepth = state.order_depths[AMETHYSTS]
         orders_to_place: list[Order] = []
-
-        # Sort sell and buy orders by price
-        osell = collections.OrderedDict(sorted(order_depth.sell_orders.items()))
-        obuy = collections.OrderedDict(sorted(order_depth.buy_orders.items(), reverse=True))
-
-        _, best_sell_pr = self.extract_market_values(osell)
-        _, best_buy_pr = self.extract_market_values(obuy, is_buy_order=True)
-
+        sell_orders = collections.OrderedDict(sorted(order_depth.sell_orders.items()))
+        buy_orders = collections.OrderedDict(sorted(order_depth.buy_orders.items(), reverse=True))
         current_position = self.position[AMETHYSTS]
 
-        mx_with_buy = -1
+        # Evaluate each sell order to determine if buying them is beneficial
+        for selling_price, volume_offered in sell_orders.items():
+            is_good_deal = selling_price < 10000
+            is_position_short = self.position[AMETHYSTS] < 0 and selling_price == 10000
+            within_position_limit = current_position < POSITION_LIMIT[AMETHYSTS]
 
-        # Process potential buy orders based on sorted sell orders and current position.
-        for ask, vol in osell.items():
-            if ((ask < 10000) or ((self.position[AMETHYSTS]<0) and (ask == 10000))) and current_position < POSITION_LIMIT[AMETHYSTS]:
-                mx_with_buy = max(mx_with_buy, ask)
-                order_for = min(-vol, POSITION_LIMIT[AMETHYSTS] - current_position)
+            if (is_good_deal or is_position_short) and within_position_limit:
+
+                order_for = min(-volume_offered, POSITION_LIMIT[AMETHYSTS] - current_position)
                 current_position += order_for
-                assert(order_for >= 0)
-                orders_to_place.append(Order(AMETHYSTS, ask, order_for))
+                assert(order_for >= 0), "Volume to buy should be non-negative"
 
-        undercut_buy = best_buy_pr + 1
-        undercut_sell = best_sell_pr - 1
+                orders_to_place.append(Order(AMETHYSTS, selling_price, order_for))
 
-        bid_pr = min(undercut_buy, 9999)
-        sell_pr = max(undercut_sell, 10001)
-
-        if (current_position < POSITION_LIMIT[AMETHYSTS]) and (self.position[AMETHYSTS] < 0):
-            order_volume = min(40, POSITION_LIMIT[AMETHYSTS] - current_position)
-            orders_to_place.append(Order(AMETHYSTS, min(undercut_buy + 1, 9999), order_volume))
-            current_position += order_volume
-
-        if (current_position < POSITION_LIMIT[AMETHYSTS]) and (self.position[AMETHYSTS] > 15):
-            order_volume = min(40, POSITION_LIMIT[AMETHYSTS] - current_position)
-            orders_to_place.append(Order(AMETHYSTS, min(undercut_buy - 1, 9999), order_volume))
-            current_position += order_volume
+        market_buy_price = calculate_vwap(buy_orders)
 
         if current_position < POSITION_LIMIT[AMETHYSTS]:
             order_volume = min(40, POSITION_LIMIT[AMETHYSTS] - current_position)
-            orders_to_place.append(Order(AMETHYSTS, bid_pr, order_volume))
+
+            buy_price_undercut = 1
+            # Adjust the undercut of the buy price based on the position
+            if self.position[AMETHYSTS] < 0:
+                buy_price_undercut = 2  # Attempt to cover short position more aggressively
+            elif self.position[AMETHYSTS] > 15:
+                buy_price_undercut = 0  # No additional adjustment, use market_buy_price directly
+
+            buy_price = min(market_buy_price + buy_price_undercut, 9999)
+            orders_to_place.append(Order(AMETHYSTS, buy_price, order_volume))
             current_position += order_volume
-        
+
+        #reset 
         current_position = self.position[AMETHYSTS]
 
-        for bid, vol in obuy.items():
-            if ((bid > 10000) or ((self.position[AMETHYSTS]>0) and (bid == 10000))) and current_position > -POSITION_LIMIT[AMETHYSTS]:
-                order_volume = max(-vol, -POSITION_LIMIT[AMETHYSTS]-current_position)
+        market_sell_price = calculate_vwap(sell_orders)
+        for bid_price, volume_demanded in buy_orders.items():
+            is_above_minimum = bid_price > 10000
+            is_position_long = (self.position[AMETHYSTS] > 0) and (bid_price == 10000)
+            within_position_limit = current_position > -POSITION_LIMIT[AMETHYSTS]
+
+            if (is_above_minimum or is_position_long) and within_position_limit:
+                # Determine the maximum volume to order without exceeding the position limit
+                order_volume = max(-volume_demanded, -POSITION_LIMIT[AMETHYSTS] - current_position)
                 current_position += order_volume
-                assert(order_volume <= 0)
-                orders_to_place.append(Order(AMETHYSTS, bid, order_volume))
 
-        if (current_position > -POSITION_LIMIT[AMETHYSTS]) and (self.position[AMETHYSTS] > 0):
-            order_volume = max(-40, -POSITION_LIMIT[AMETHYSTS]-current_position)
-            orders_to_place.append(Order(AMETHYSTS, max(undercut_sell-1, 10001), order_volume))
-            current_position += order_volume
+                assert(order_volume <= 0), "Volume to sell should be non-positive"
 
-        if (current_position > -POSITION_LIMIT[AMETHYSTS]) and (self.position[AMETHYSTS] < -15):
-            order_volume = max(-40, -POSITION_LIMIT[AMETHYSTS]-current_position)
-            orders_to_place.append(Order(AMETHYSTS, max(undercut_sell+1, 10001), order_volume))
-            current_position += order_volume
-
+                orders_to_place.append(Order(AMETHYSTS, bid_price, order_volume))
+            
         if current_position > -POSITION_LIMIT[AMETHYSTS]:
-            order_volume = max(-40, -POSITION_LIMIT[AMETHYSTS]-current_position)
-            orders_to_place.append(Order(AMETHYSTS, sell_pr, order_volume))
+            order_volume = max(-40, -POSITION_LIMIT[AMETHYSTS] - current_position)
+            
+            undercut_sell_undercut = -1
+            # Adjust the undercut_sell based on the position
+            if self.position[AMETHYSTS] > 0:
+                undercut_sell_undercut = -2
+            elif self.position[AMETHYSTS] < -15:
+                undercut_sell_undercut = 0  # No adjustment, use market_sell_price directly
+
+            sell_price = max(market_sell_price + undercut_sell_undercut, 10001)
+
+            orders_to_place.append(Order(AMETHYSTS, sell_price, order_volume))
             current_position += order_volume
 
         return orders_to_place
@@ -138,7 +121,7 @@ class Trader:
                     orders.append(Order(STARFRUIT, best_asks[i], -best_ask_volume))
                     prod_position += -best_ask_volume
                 else:
-                    # Buy as much as we can without exceeding the self.max_pos[product]
+                    # Buy as much as we can without exceeding the max_pos
                     vol = max_pos - prod_position
                     orders.append(Order(STARFRUIT, best_asks[i], vol))
                     prod_position += vol
@@ -156,7 +139,7 @@ class Trader:
                     orders.append(Order(STARFRUIT, best_bids[i], -best_bid_volume))
                     prod_position += -best_bid_volume
                 else:
-                    # Sell as much as we can without exceeding the self.max_pos[product]
+                    # Sell as much as we can without exceeding the max_pos
                     vol = prod_position + max_pos
                     orders.append(Order(STARFRUIT, best_bids[i], -vol))
                     prod_position += -vol
@@ -204,3 +187,28 @@ class Trader:
         conversions = 1 
  
         return result, conversions, traderData
+    
+
+#Utils for Amnethysts
+def calculate_vwap(orders):
+    """
+    Calculates the Volume Weighted Average Price (VWAP) for a collection of orders.
+    Orders can be either buy or sell orders. For sell orders, the quantities are negative.
+    """
+    total_value = 0
+    total_volume = 0
+
+    for price, volume in orders.items():
+        # For sell orders, volume is negative, so we negate it again to make it positive for calculation
+        adjusted_volume = abs(volume)
+        total_value += price * adjusted_volume
+        total_volume += adjusted_volume
+
+    # Ensure total_volume is not zero to avoid division by zero error
+    if total_volume > 0:
+        vwap = total_value / total_volume
+    else:
+        vwap = 0
+
+    return round(vwap)
+
